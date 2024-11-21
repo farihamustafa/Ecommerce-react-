19ee1c81cab35c210806a71f83dd196bcccbe396
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-11-21 10:37:51 +0500

    ...

--- ecommerce/src/layouts/Guestlayout.jsx
+++ ecommerce/src/layouts/Guestlayout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import Fnavbar from '../components/Fnavbar';
 
 function Guestlayout(props) {
     const {children} = props;
   return (
    <>
-    <div>
-      Guestlayout
-    </div>
+   <Fnavbar/>
     {children}
+    <Ffooter/>
    </>
   )
 }
