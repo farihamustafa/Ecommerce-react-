138193259b0eb6c2cd5580eb5cb6293af5b7abd4
asp <asp@aptechnn2.edu>
2024-11-28 10:38:08 +0500

    updated

--- ecommerce/src/layouts/Authlayout.jsx
+++ ecommerce/src/layouts/Authlayout.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { authService } from '../services/authService';
+import { useNavigate } from 'react-router-dom';
+const Token = localStorage.getItem('token');
 
 function Authlayout(props) {
-    const {children} = props
+    const {children} = props;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const verifyToken = async () => {
+        if (!Token) {
+          navigate('/login');
+        } else {
+          const { result, error } = await authService.verify(Token);
+          if (error) {
+            navigate('/login');
+          }
+        }
+      };
+  
+      verifyToken();
+    }, []);
+  
+
+
   return (
     <>
-    <div>
-      Authlayout
-    </div>
+    <div>Authlayout</div>
     {children}
     </>
   )
--- ecommerce/src/pages/Login.jsx
+++ ecommerce/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { authService } from './services/authService'
+import { authService } from '../services/authService'
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup'
 import { toast } from 'react-toastify';
